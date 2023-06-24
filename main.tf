fcebaf294e78de90d7656f3533f3c45dc7174d82
rekeyole <rekeyole@DESKTOP-FKEVB4C>
2023-06-24 20:45:49 +0200

    updating module

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Calling our Module here
 
 module "eks-module" {
-  source         = "github.com/ooghenekaro/my-eks-module"
+  source         = "./modules"
   region         = "eu-west-2"
   vpc_cidr       = "10.0.0.0/16"
   dns_hostnames  = true
